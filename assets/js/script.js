45dd6b04d7303800fa465a3a1f9282bb94a9a5a5
Holly Horwood <holly.horwood@live.com>
2019-06-03 00:59:21 +0000

    Got map to display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
-$(function(){
-      $('#map').vectorMap({map: 'nz_mill'});
-    });
+// initialize the map on the "map" div with a given center and zoom
+var mymap = L.map('mapContainer').setView([40.9006, -174.8860], 10);
 
+var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+	maxZoom: 18
+}).addTo(mymap);
 
+var marker = L.marker([41.2784, 174.7767]).addTo(mymap)
+.bindPopup('The Beehive Wellington, house of Parliment')
