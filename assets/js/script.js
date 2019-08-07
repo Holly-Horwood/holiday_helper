3acfeac11170a1dca114d46ad44eef827dc240fb
Holly Horwood <holly.horwood@live.com>
2019-08-07 16:09:22 +1200

    added non functioning marker clusters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 const apiKey = "AGzltg49Lx3bVQJdJjToy20ssY8ep6iJ9ai504vK";
 const xhr = new XMLHttpRequest();
 const newZealand = { lat: -40.9006, lng: 174.886 };
-const putaruru = { lat: -38.038500, lng: 175.792754 };     
+const putaruru = { lat: -38.0385, lng: 175.792754 };
+const ashburton = { lat: -43.631001, lng: 170.998599 };
 var marker;
 var map;
 
@@ -243,6 +244,18 @@ function initMap() {
       }
     ]
   });
+  // Create an array of alphabetical characters used to label the markers.
+  var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  // Add some markers to the map.
+  var markers = marker.map(function(marker, i) {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length]
+    });
+  });
+  // Add a marker clusterer to manage the markers.
+  var markerCluster = new MarkerClusterer(map, markers,
+      {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 }
 
 // Loops through json data relevant to search criteria selected, zooms in on Island and shows site markers
@@ -256,23 +269,20 @@ rc.onclick = searchSelect;
 function searchSelect(e) {
   if (rc.checked) {
     if (di.value == "ni") {
-      console.log("north island + campsites");
-
       //Calls to DOC API and returns campsite data
       var iconTent = {
         url: "assets/images/tent.png",
         scaledSize: new google.maps.Size(20, 20)
       };
-      TODO:
-      var infoWindow = new google.maps.InfoWindow();
+      TODO: var infoWindow = new google.maps.InfoWindow();
 
       xhr.open("GET", "https://api.doc.govt.nz/v2/campsites", true);
       xhr.setRequestHeader("X-API-Key", apiKey);
 
       xhr.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
-            map.setZoom(7);
-            map.setCenter(putaruru);
+          map.setZoom(7);
+          map.setCenter(putaruru);
 
           let json = JSON.parse(this.responseText);
           //loops through campsite json data and returns campsite information and adds to map
@@ -292,12 +302,117 @@ function searchSelect(e) {
       xhr.send();
     } else if (di.value == "si") {
       console.log("south island + campsites");
+      //Calls to DOC API and returns campsite data
+      var iconTent = {
+        url: "assets/images/tent.png",
+        scaledSize: new google.maps.Size(20, 20)
+      };
+      TODO: var infoWindow = new google.maps.InfoWindow();
+
+      xhr.open("GET", "https://api.doc.govt.nz/v2/campsites", true);
+      xhr.setRequestHeader("X-API-Key", apiKey);
+
+      xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          map.setZoom(7);
+          map.setCenter(ashburton);
+
+          let json = JSON.parse(this.responseText);
+          //loops through campsite json data and returns campsite information and adds to map
+          for (let i = 0; i < json.length; i++) {
+            let campsite = json[i];
+            console.log(campsite.name + "," + campsite.x + "," + campsite.y);
+            let latLong = convertToLatLong(campsite.x, campsite.y);
+            marker = new google.maps.Marker({
+              position: new google.maps.LatLng(latLong[0], latLong[1]),
+              map: map,
+              icon: iconTent
+            });
+          }
+        }
+      };
+
+      xhr.send();
     }
   } else if (rh.checked) {
     if (di.value == "ni") {
       console.log("north island + huts");
+      //Calls to DOC API and returns campsite data
+      var iconHut = {
+        url: "assets/images/hut.png",
+        scaledSize: new google.maps.Size(20, 20)
+      };
+      TODO: var infoWindow = new google.maps.InfoWindow();
+
+      xhr.open("GET", "https://api.doc.govt.nz/v2/huts", true);
+      xhr.setRequestHeader("X-API-Key", apiKey);
+
+      xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          map.setZoom(7);
+          map.setCenter(putaruru);
+
+          let json = JSON.parse(this.responseText);
+          //loops through campsite json data and returns campsite information and adds to map
+          for (let i = 0; i < json.length; i++) {
+            let campsite = json[i];
+            console.log(campsite.name + "," + campsite.x + "," + campsite.y);
+            let latLong = convertToLatLong(campsite.x, campsite.y);
+            marker = new google.maps.Marker({
+              position: new google.maps.LatLng(latLong[0], latLong[1]),
+              map: map,
+              icon: iconHut
+            });
+          }
+        }
+      };
+
+      xhr.send();
     } else if (di.value == "si") {
       console.log("south island + huts");
+      //Calls to DOC API and returns campsite data
+      var iconHut = {
+        url: "assets/images/hut.png",
+        scaledSize: new google.maps.Size(20, 20)
+      };
+      TODO: var infoWindow = new google.maps.InfoWindow();
+
+      
+
+      xhr.open("GET", "https://api.doc.govt.nz/v2/huts", true);
+      xhr.setRequestHeader("X-API-Key", apiKey);
+
+      xhr.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          map.setZoom(7);
+          map.setCenter(ashburton);
+
+          let json = JSON.parse(this.responseText);
+          //loops through campsite json data and returns campsite information and adds to map
+          for (let i = 0; i < json.length; i++) {
+            let campsite = json[i];
+            console.log(campsite.name + "," + campsite.x + "," + campsite.y);
+            let latLong = convertToLatLong(campsite.x, campsite.y);
+            marker = new google.maps.Marker({
+              position: new google.maps.LatLng(latLong[0], latLong[1]),
+              map: map,
+              icon: iconHut
+            });
+            map.setMapTypeId('terrain');
+          }
+        }
+      };
+
+      xhr.send();
+      
     }
   } else console.log("No action selected");
+}
+
+function hutSi() {
+  if (rh.checked) {
+    if (di.value == "si") {
+      
+    }
+  }
 }
